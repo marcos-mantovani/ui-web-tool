2ebe572d8c92f822436f78ebe32cf4982d628f97
Marcos Mantovani <marcosg056@gmail.com>
2024-06-03 19:35:24 -0300

    Features

--- app/page.js
+++ app/page.js
@@ -1,10 +1,12 @@
 import Layout from '/components/Layout';
 import Banner from '/components/Home/Banner';
+import Features from '/components/Home/Features';
 
 export default function Home() {
   return (
     <Layout>
       <Banner />
+      <Features />
     </Layout>
   );
 }
---
+++ components/Home/Features/index.js
@@ -0,0 +1,111 @@
+import {Container, Row, Col} from 'react-bootstrap';
+import Image from 'next/image';
+
+import SectionTitle from '../../SectionTitle';
+
+import styles from "./index.module.css";
+
+export default function Features() {
+  return (
+    <Container className={styles.features}>
+      <SectionTitle title="Avalie e Melhore a UI da sua Interface com Facilidade" />
+      <Row className="justify-content-center">
+        <Col xs={4}>
+          <div className={styles.card}>
+            <div className="text-center">
+              <Image 
+                src="/icons/thumbUp.png"
+                width={35}
+                height={35}
+                title="Facilidade de uso"
+                alt="Facilidade de uso" 
+              />
+            </div>
+            <div className={styles.titleCard}>
+              Facilidade de uso
+            </div>
+            <div className={styles.textCard}>
+              Interaja com uma interface intuitiva e responda a um questionário guiado sem complicações.
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div className={styles.card}>
+            <div className="text-center">
+              <Image 
+                src="/icons/fastForward.png"
+                width={35}
+                height={35}
+                title="Avaliação rápida"
+                alt="Avaliação rápida" 
+              />
+            </div>
+            <div className={styles.titleCard}>
+              Avaliação rápida
+            </div>
+            <div className={styles.textCard}>
+              Receba uma análise completa da sua interface em minutos, sem perder tempo.
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div className={styles.card}>
+            <div className="text-center">
+              <Image 
+                src="/icons/requestQuote.png"
+                width={35}
+                height={35}
+                title="Gratuito"
+                alt="Gratuito" 
+              />
+            </div>
+            <div className={styles.titleCard}>
+              Gratuito
+            </div>
+            <div className={styles.textCard}>
+              Acesse todas as funcionalidades da ferramenta sem nenhum custo.
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div className={styles.card}>
+            <div className="text-center">
+              <Image 
+                src="/icons/laptop.png"
+                width={35}
+                height={35}
+                title="Amplamente abrangente"
+                alt="Amplamente abrangente" 
+              />
+            </div>
+            <div className={styles.titleCard}>
+              Amplamente abrangente
+            </div>
+            <div className={styles.textCard}>
+              Compatível com uma ampla variedade de interfaces, cobrindo todos os aspectos cruciais de UI.
+            </div>
+          </div>
+        </Col>
+        <Col xs={4}>
+          <div className={styles.card}>
+            <div className="text-center">
+              <Image 
+                src="/icons/assessment.png"
+                width={35}
+                height={35}
+                title="Relatório detalhado"
+                alt="Relatório detalhado" 
+              />
+            </div>
+            <div className={styles.titleCard}>
+              Relatório detalhado
+            </div>
+            <div className={styles.textCard}>
+              Obtenha um relatório detalhado com a pontuação e sugestões práticas para melhorias específicas.
+            </div>
+          </div>
+        </Col>
+      </Row>
+    </Container>
+  );
+}
